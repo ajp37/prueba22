db8f32ac034d2d1ef90586fce19f723030befd1e
ajp37 <127146489+ajp37@users.noreply.github.com>
2026-02-16 19:57:24 +0100

    Actualizar script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+const filterButtons = document.querySelectorAll(".filter-btn");
+const menuItems = document.querySelectorAll(".menu-item");
 
+filterButtons.forEach(button => {
+  button.addEventListener("click", () => {
+
+    document.querySelector(".filter-btn.active").classList.remove("active");
+    button.classList.add("active");
+
+    const filter = button.getAttribute("data-filter");
+
+    menuItems.forEach(item => {
+      if(filter === "all"){
+        item.style.display = "block";
+      } else {
+        if(item.classList.contains(filter)){
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
+      }
+    });
+
+  });
+});
